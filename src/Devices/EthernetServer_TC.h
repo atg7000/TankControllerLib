fafdc212a6bdf245a1586359f91b7676ea5c6067
Austin Garner <austin.garner@wallawalla.edu>
2020-11-26 11:51:24 -0800

    Adding wrapper class for EthernetServer

---
+++ src/Devices/EthernetServer_TC.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <Arduino.h>
+#ifdef MOCK_PINS_COUNT
+#include <Ethernet_CI.h>
+#else
+#include <Ethernet.h>
+#endif
+
+// If running in test mode, superclass = EthernetServer_CI
+class EthernetServer_TC : public EthernetServer {
+public:
+  // class methods
+  static EthernetServer_TC* instance();
+  // instance methods
+  void handleRequest();
+  virtual String className() const {
+    return "EthernetServer_TC";
+  }
+
+private:
+  // class variables
+  static EthernetServer_TC* _instance;
+
+  // instance methods
+  EthernetServer_TC(uint16_t port);
+};
---
+++ test/EthernetServer.cpp
@@ -0,0 +1,15 @@
+#include <Arduino.h>
+#include <ArduinoUnitTests.h>
+
+#include <string>
+#include <vector>
+
+#include "TankControllerLib.h"
+
+unittest(test) {
+    EthernetServer_TC* server = EthernetServer_TC::instance();
+    assertEqual("EthernetServer_TC", server->className());
+    assertEqual(80, server->getPort());
+}
+
+unittest_main()
