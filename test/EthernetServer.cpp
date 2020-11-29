7c948eab4416d4c5b3b4fcfbf84f6578f5cbb1b0
James Foster <github@jgfoster.net>
2020-11-29 09:28:40 -0800

    Update EthernetServer.cpp
    Add comments in Doxygen format.

--- test/EthernetServer.cpp
+++ test/EthernetServer.cpp
@@ -6,6 +6,10 @@
 
 #include "TankControllerLib.h"
 
+/**
+ * Trivial test to confirm that EthernetServer_TC compiles,
+ * is of proper class, and has proper port
+ */
 unittest(test) {
   EthernetServer_TC* server = EthernetServer_TC::instance();
   assertEqual("EthernetServer_TC", server->className());
